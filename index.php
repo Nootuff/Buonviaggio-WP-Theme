37292e572d515af7155f2e68e811560d19041ef6
Adam Walker <adamwalkerlondon@gmail.com>
2023-01-27 18:42:06 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    
+    
+    <head>
+    <meta charset="utf-8">
+  <title>Buonviaggio</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       
+       <!-- <link rel="stylesheet" href="newcss.css"> -->
+    </head>
+    <body>
+<main>
+  <section>
+<h1>Read your Semantic notes and plan out what each section will be</h1>
+<p>Don't just have a bunch of divs, <a href="https://www.w3schools.com/html/html5_semantic_elements.asp">use these</a> .</p>
+</section>
+</main>
+      <!--  <script type="text/javascript" src="newjavascript.js"></script> -->
+    </body>
+    </html>
